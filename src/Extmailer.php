71a377d78b1cd4153bd717eb6326c184dff176cf
chudaster <chudaster@yandex.ru>
2020-06-05 22:54:06 +0300

    Update and rename Extmailer.php to Mailer.php

--- src/Extmailer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Chudaster\Service;
-
-class Extmailer {
-    public static function send() {
-        echo 'helo';
-    }
-}
---
+++ src/Mailer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Chudaster\Service;
+
+class Mailer extends \Illuminate\Mail\Mailer {
+    public static function sendx() {
+        echo 'helo';
+    }
+}
